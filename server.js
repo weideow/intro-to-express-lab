15a45c40127c43e09557e2a0d865ddcb7f5fe5ac
weideow <owweide@hotmail.com>
2025-02-06 08:46:10 +0800

    stuck qns4

--- server.js
+++ server.js
@@ -11,21 +11,21 @@ app.get('/', (req, res) => {
     res.send(`Home Page`);
 })
 
-// app.get('/:greetings/:name', (req, res) => {
-//     res.send(`${req.params.greetings}, ${req.params.name}`);
-//     console.log(req.params.greetings);
-// });
+app.get('/:greetings/:name', (req, res) => {
+    res.send(`${req.params.greetings}, ${req.params.name}`);
+    console.log(req.params.greetings);
+});
 
-// app.get('/roll/:number', (req, res) => {
-//     const number = req.params.number;
+app.get('/roll/:number', (req, res) => {
+    const number = req.params.number;
 
-//     // Check if the number parameter is undefined or an empty string
-//   if (isNaN(number)) {
-//         res.send('You must specify a valid number');
-//     } else {
-//         res.send(`You rolled a ${Math.floor(Math.random()*Number(number)+1)}`);
-//     }
-// });
+    // Check if the number parameter is undefined or an empty string
+  if (isNaN(number)) {
+        res.send('You must specify a valid number');
+    } else {
+        res.send(`You rolled a ${Math.floor(Math.random()*Number(number)+1)}`);
+    }
+});
 
 app.get('/collectibles/:number', (req, res) => {
 
